67c4640149797dcb65c18203c17ce4d9e7b06423
吴紫葳 <1037712339@qq.com>
2020-07-15 14:12:00 +0800

    测试

--- lib/login.py
+++ lib/login.py
@@ -2,22 +2,39 @@
 # times:2020/7/9 9:24
 # # coding:-*- utf-8 -*-
 
+import time
 from selenium import webdriver
 
 driver = webdriver.Chrome(r'D:\weican_web_aotu\chromedriver.exe')
 
+# 门店相关信息
 merchant_id = '539848'
 account = 'wct'
 password = '123456'
 driver.get('http://b.local.weicantimes.com/login')
+driver.maximize_window()
+
+time.sleep(2)
+# 输入门店相关登录信息
+input = driver.find_elements_by_css_selector('input.login-input-full')
+for i in input:
+    if '请输入商家编号' in i.get_attribute('placeholder'):
+        i.send_keys(merchant_id)
+    elif '请输入账号' in i.get_attribute('placeholder'):
+        i.send_keys(account)
+    elif '请输入密码' in i.get_attribute('placeholder'):
+        i.send_keys(password)
+        break
+# 点击登录
+driver.find_element_by_css_selector('input.login-form-login').click()
+
+#循环点击title的每一个选项
+time.sleep(3)
+title = driver.find_elements_by_css_selector('div.navbar-contents a')
+for i in title:
+    time.sleep(1)
+    i.click()
 
 
-def login():
-    driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[1]/div[2]/form/div[1]/div/input').send_keys(merchant_id)
-    driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[1]/div[2]/form/div[2]/div/input').send_keys(account)
-    driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[1]/div[2]/form/div[3]/div/input').send_keys(password)
-    driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[1]/div[2]/form/div[4]/div/input').send_keys('1')
-    driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[1]/div[2]/form/div[6]/div[1]/input').click()
-
-
-login()
+time.sleep(5)
+driver.quit()
